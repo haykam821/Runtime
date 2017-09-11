9587a3559d0dc1d1ae76f0e535e6f5cb40e3f85b
haykam821 <haykam821@users.noreply.github.com>
2017-09-11 11:30:23 -0400

    Custom prefix support

--- Runtime.obp.js
+++ Runtime.obp.js
@@ -4,11 +4,11 @@ exports.onMessageReceived = (function Runtime(bot, doc, user, userID, channelID,
     if(message === undefined){
         return;
     }
-    if (message === "*sincestart" || message === "*runtime" || message === "*uptime" || message === "*upfor" || message === "*sincestartup" || message === "*runtimer") {
+    if (message === doc.prefix + "sincestart" || message === doc.prefix + "runtime" || message === doc.prefix + "uptime" || message === doc.prefix + "upfor" || message === doc.prefix + "sincestartup" || message === doc.prefix + "runtimer") {
         var end = new Date() - start;
         bot.sendMessage({
             to: doc.logchannel,
-            message: user + " asked for how long the bot has been running for since its last startup (`" + end + "` milliseconds)"
+            message: user + " asked for how long the bot has been running for since its last startup (`" + end + "` milliseconds)."
         });
         bot.sendMessage({
             to: channelID,
